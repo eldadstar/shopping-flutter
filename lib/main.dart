3fc51c9def7a3bbde23fc18cdfe4bb8db67212d0
eldadstar <64614286+eldadstar@users.noreply.github.com>
2021-06-11 07:36:49 +1000

    day 9 completed

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
         fontFamily: GoogleFonts.lato().fontFamily,
       ),
+      debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch: Colors.green,
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  final imageUrl =
+      "https://www.learnreligions.com/thmb/fYKu1meMfGEFkqj8HaLO0su0Ee8=/1500x844/smart/filters:no_upscale()/JesusChristGettyImages-600166818-5a4e7131eb4d520037d96847.jpg";
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                accountName: Text("eldad fikre"),
+                accountEmail: Text("eldadfikre456@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                ),
+              ),
+            ),
+            ListTile(
+                leading: Icon(
+                  CupertinoIcons.home,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Home",
+                  textScaleFactor: 1.2,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                )),
+            ListTile(
+                leading: Icon(
+                  CupertinoIcons.profile_circled,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Profile",
+                  textScaleFactor: 1.2,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                )),
+            ListTile(
+                leading: Icon(
+                  CupertinoIcons.mail,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Email me",
+                  textScaleFactor: 1.2,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                )),
+            ListTile(
+                leading: Icon(
+                  CupertinoIcons.home,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Home",
+                  textScaleFactor: 1.2,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                )),
+            ListTile(
+                leading: Icon(
+                  CupertinoIcons.home,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Home",
+                  textScaleFactor: 1.2,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
