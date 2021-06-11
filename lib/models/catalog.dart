5d7e2adabd28c60f1c942d62bd151ce483c68f56
eldadstar <64614286+eldadstar@users.noreply.github.com>
2021-06-11 23:23:26 +1000

    day 12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,21 +1,29 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "iphone 12 Pro",
+        desc: "Apple iphone 12th generation",
+        price: 999,
+        color: "#33505a",
+        image:
+            "https://www.mytrendyphone.eu/images/iPhone-12-Pro-Max-128GB-Graphite-0194252021200-25102020-1-p.jpg")
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
   final String color;
   final String image;
 
-  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
-}
-
-final products = [
   Item(
-      id: "Codepur001",
-      name: "iphone 12 Pro",
-      desc: "Apple iphone 12th generation",
-      price: 999,
-      color: "#33505a",
-      image:
-          "https://www.mytrendyphone.eu/images/iPhone-12-Pro-Max-128GB-Graphite-0194252021200-25102020-1-p.jpg")
-];
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
